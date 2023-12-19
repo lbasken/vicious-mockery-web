be46b7af41e3e53420e7a47dfeb8279b500dcbb0
Lorelei <lorelei@basken.com>
2023-12-18 20:59:17 -0500

    added AppBar styles, created About and API Doc pages

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,7 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import React from 'react';
-
 export default function AppBar() {
-    return <div className="bg-red-500 h-10 px-20 pt-1.5">Vicious Mockery Insult API</div>
+    return <div className="bg-purple-900 h-10 pt-1.5">
+        <section className="flex flex-row-reverse">
+            <div className="pr-28"><a href="https://google.com">API Docs</a></div>
+            <div className="pr-8"><a href="https://google.com">About</a></div>
+            <div className="pr-96">Vicious Mockery Insult API</div>
+        </section>
+    </div>
+
 }
