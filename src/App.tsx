3be2570072397990b1254d8396a70479e9ac2cd6
Lorelei <lorelei@basken.com>
2023-12-18 21:33:59 -0500

    defined route paths, added styling to App Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css';
 import AppBar from "./components/AppBar/AppBar.tsx";
+import HomePage from "./pages/HomePage/HomePage.tsx";
+import AboutPage from "./pages/AboutPage/AboutPage.tsx";
+import DocPage from "./pages/DocPage/DocPage.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 
-  return <div>
-    <AppBar />
-    <div>Hello World</div>
-  </div>
+  return <BrowserRouter>
+    <AppBar/>
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/about" element={<AboutPage />}/>
+      <Route path="/api" element={<DocPage />}/>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,8 +1,10 @@
+import {Link} from "react-router-dom";
+
 export default function AppBar() {
     return <div className="bg-purple-900 h-10 pt-1.5">
         <section className="flex flex-row-reverse">
-            <div className="pr-28"><a href="https://google.com">API Docs</a></div>
-            <div className="pr-8"><a href="https://google.com">About</a></div>
+            <div className="pr-28"><Link to="/api">API Docs</Link></div>
+            <div className="pr-8"><Link to="/about">About</Link></div>
             <div className="pr-96">Vicious Mockery Insult API</div>
         </section>
     </div>
