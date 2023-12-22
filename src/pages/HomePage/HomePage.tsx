450d8330cd45256765d6caa35c1053f18bfe9e72
Lorelei <lorelei@basken.com>
2023-12-21 20:36:38 -0500

    implemented  continuous vicious mockery insult generation

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,19 +4,32 @@ import {useState} from "react";
 export default function HomePage() {
 
     const [show, setShow] = useState(false);
+    const[render, setRender] = useState(false);
 
     function onClick() {
+        setRender(false);
         setShow(true);
     }
 
-    if(show) {
+    function onTest() {
+        setRender(true);
+    }
+
+    if(render) {
         return <div>
             <MockGenerator />
-            <button>Click Me</button>
+            <button onClick={onClick}>Button</button>
         </div>
     }
 
-    return <div>
-        <button onClick={onClick}>Click Me</button>
-    </div>
+    if(!show) {
+        return <div>
+            <button onClick={onClick}>Click Me</button>
+        </div>
+    } else {
+        return <div>
+            <MockGenerator />
+            <button onClick={onTest}>Click Me Again</button>
+        </div>
+    }
 }
