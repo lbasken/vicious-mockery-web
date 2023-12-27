94ceb406abb3cf77a4b6602123f6d493fd64e1b5
Lorelei <lorelei@basken.com>
2023-12-27 17:43:31 -0500

    added styles to AppBar, continuing to refine vicious mockery insult generation

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -3,10 +3,9 @@ import {Link} from "react-router-dom";
 export default function AppBar() {
     return <div className="bg-purple-900 h-10 pt-1.5">
         <section className="flex flex-row-reverse">
-            <div className="pr-28"><Link to="/api">API Docs</Link></div>
-            <div className="pr-8"><Link to="/about">About</Link></div>
-            <div className="pr-96">Vicious Mockery Random Insult Generator</div>
+            <div className="pr-28 text-white"><Link to="/api">API Docs</Link></div>
+            <div className="pr-8 text-white"><Link to="/about">About</Link></div>
+            <div className="pr-96 text-white"><Link to="/">Vicious Mockery Random Insult Generator</Link></div>
         </section>
     </div>
-
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,34 +2,16 @@ import MockGenerator from "../../components/MockGenerator/MockGenerator.tsx";
 import {useState} from "react";
 
 export default function HomePage() {
+    const [version, setVersion] = useState(0);
 
-    const [show, setShow] = useState(false);
-    const[render, setRender] = useState(false);
-
-    function onClick() {
-        setRender(false);
-        setShow(true);
+    function handleReset() {
+        setVersion(version + 1);
     }
 
-    function onTest() {
-        setRender(true);
-    }
-
-    if(render) {
-        return <div>
-            <MockGenerator />
-            <button onClick={onClick}>Button</button>
-        </div>
-    }
-
-    if(!show) {
-        return <div>
-            <button onClick={onClick}>Click Me</button>
-        </div>
-    } else {
-        return <div>
-            <MockGenerator />
-            <button onClick={onTest}>Click Me Again</button>
-        </div>
-    }
+    return (
+        <>
+            <button onClick={handleReset}>Reset</button>
+            <MockGenerator key={version} />
+        </>
+    );
 }
