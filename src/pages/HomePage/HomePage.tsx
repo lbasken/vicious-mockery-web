593d0d13c254fe61fd563a35d3ccf6fc1b04911f
Lorelei <lorelei@basken.com>
2023-12-30 17:09:38 -0500

    mock button generator working as expected, removed unneeded code

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,35 +3,39 @@ import {useEffect, useState} from "react";
 export default function HomePage(): any {
 
     const [mocks, setMocks] = useState<any>([]);
-    const [clicked, setClicked] = useState<boolean>(false);
+    const [mock, setMock] = useState<string>();
+
+    console.log("HomePage running");
+    console.log("mocks", mocks);
+    console.log("mock", mock);
 
     useEffect(() => {
         fetch("https://api-rkstgpevmq-uc.a.run.app/mock")
             .then(response => response.json())
             .then(items => {
-                console.log("items:", items);
+                // console.log("items:", items);
                 setMocks(items);
             });
     }, []);
 
+    // useEffect(() => {
+    //     getMock();
+    // }, [mocks]);
+
     function getMock(): any {
+        if (!mocks?.length) { return; }
         const randomMock: any = mocks[Math.floor(Math.random() * mocks.length)];
-        console.log("random mock:", randomMock);
-        return randomMock?.mock;
+        setMock(randomMock?.mock);
     }
 
     function onClick(): any {
-        setClicked(true);
+        getMock();
     }
 
-    if(clicked) {
-        return <div>
-            {getMock()}
-            <div><button onClick={getMock}>Another!</button></div>
+    return <div>
+        <div>
+            <button onClick={onClick}>Mock me!</button>
+            <div>{mock}</div>
         </div>
-    } else {
-        return <div>
-            <button onClick={onClick}>Click Me!</button>
-        </div>
-    }
+    </div>
 }
